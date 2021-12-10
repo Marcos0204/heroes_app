f838d4252d22263c781bef7f30f8927ad4c11fa1
Marcos Bastidas <marcosbastidas301@gmail.com>
2021-12-10 18:14:53 -0400

    update-card-components-show

--- src/components/hero/HeroList.jsx
+++ src/components/hero/HeroList.jsx
@@ -1,16 +1,14 @@
 import React from 'react'
 import { getHeroesByPublisher } from '../../selectores/getHeroesByPublisher'
+import { HeroCard } from './HeroCard'
 
 export const HeroList = ({publisher}) => {
     const heroes = getHeroesByPublisher(publisher)
     return (
-        <>
-            <h2>Hero List - {publisher} </h2>
-            <ul>
-                {
-                    heroes.map(heroe => <li key={heroe.id}>{heroe.superhero}</li> )
-                }
-            </ul>
-        </>
+        <div className='row rows-cols-1 row-cols-md-4 g-3 d-flex '>
+            {
+                heroes.map(heroe => <HeroCard key={heroe.id} {...heroe} /> )
+            }
+        </div>
     )
 }
